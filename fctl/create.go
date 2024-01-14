4a30957d44c767f11552a6ecb1cc2dcee4fa51da
Fan Tianhao <70184655+TTraveller7@users.noreply.github.com>
2024-01-14 17:40:34 +0800

    Implement monitor load config. Add k8s yamls.  (#8)
    * fix npr
    
    * add k8s yamls
    
    * implement addTopics fctl cmd
    
    * fill in kafka config
    
    * change replication factor

--- fctl/create.go
+++ fctl/create.go
@@ -10,12 +10,18 @@ import (
 )
 
 func Create() {
-	pathPtr := pflag.StringP("config", "c", "", "yaml config path")
+	pathPtr := pflag.StringP("config", "c", "", "Yaml config path.")
+	keepAliveOnFailurePtr := pflag.BoolP("keepAliveOnFailure", "k", false,
+		"If set to true, resources are kept alive and not removed when any creation step fails. Defualt false.")
 
 	pflag.Parse()
 	if pathPtr == nil || len(*pathPtr) == 0 {
 		logs.Printf("config path is not provided. Use -c <config path> to provide config path. ")
 		return
+	}
+	keepAliveOnFailure := true
+	if keepAliveOnFailurePtr != nil {
+		keepAliveOnFailure = *keepAliveOnFailurePtr
 	}
 
 	// TODO: check fission status
@@ -53,7 +59,7 @@ func Create() {
 		return
 	}
 	defer func() {
-		if !fissionStartSuccess {
+		if !keepAliveOnFailure && !fissionStartSuccess {
 			Run("fission", "env", "delete",
 				"--name", FissionEnv)
 		}
@@ -64,6 +70,9 @@ func Create() {
 		"--name", "monitor",
 		"--env", FissionEnv,
 		"--entrypoint", "Handler",
+		"--executortype", "newdeploy",
+		"--minscale", "1",
+		"--maxscale", "1",
 		"--src", ConcatPath(conf.MonitorDirectoryPath, "go.mod"),
 		"--src", ConcatPath(conf.MonitorDirectoryPath, "go.sum"),
 		"--src", ConcatPath(conf.MonitorDirectoryPath, "handler.go"))
@@ -72,7 +81,7 @@ func Create() {
 		return
 	}
 	defer func() {
-		if !fissionStartSuccess {
+		if !keepAliveOnFailure && !fissionStartSuccess {
 			Run("fission", "fn", "delete",
 				"--name", "monitor")
 		}
@@ -89,7 +98,7 @@ func Create() {
 		return
 	}
 	defer func() {
-		if !fissionStartSuccess {
+		if !keepAliveOnFailure && !fissionStartSuccess {
 			Run("fission", "httptrigger", "delete",
 				"--name", "monitor-load-root-config")
 		}
@@ -97,17 +106,31 @@ func Create() {
 
 	time.Sleep(3 * time.Second)
 
-	// load monitor config
 	cli := NewMonitorClient()
+
+	// load monitor config
+	logs.Printf("sending command loadRootConfig to monitor")
 	resp, err := cli.LoadRootConfig(invokerConfig)
 	if err != nil {
-		logs.Printf("load root config failed: %v", err)
+		logs.Printf("loadRootConfig failed: %v", err)
 		return
 	} else if resp.Code != invokerlib.ResponseCodes.Success {
-		logs.Printf("load root config failed: %s", resp.Message)
+		logs.Printf("loadRootConfig failed with resp: %+v", resp)
 		return
 	}
-	logs.Printf("loadRootConfig finished with message: %v", resp.Message)
+	logs.Printf("loadRootConfig finished with resp: %+v", resp)
+
+	// create topics from root config
+	logs.Printf("sending command createTopics to monitor")
+	resp, err = cli.CreateTopics()
+	if err != nil {
+		logs.Printf("createTopics failed: %v", err)
+		return
+	} else if resp.Code != invokerlib.ResponseCodes.Success {
+		logs.Printf("createTopics failed with resp: %+v", resp)
+		return
+	}
+	logs.Printf("createTopics finished with resp: %+v", resp)
 
 	fissionStartSuccess = true
 }
