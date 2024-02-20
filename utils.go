e8ee73b0c731ee8e8719b906dac42a188ba81ca8
Fan Tianhao <70184655+TTraveller7@users.noreply.github.com>
2024-02-20 19:08:50 +0800

    add utils function (#41)

--- utils.go
+++ utils.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"io"
+	"strconv"
 )
 
 // MarshalToReader transforms a struct to io.Reader.
@@ -24,4 +25,14 @@ func SafeJsonIndent(s any) string {
 		return ""
 	}
 	return string(marshalledS)
+}
+
+func BytesToUint64(bytes []byte) uint64 {
+	num, _ := strconv.ParseUint(string(bytes), 10, 64)
+	return num
+}
+
+func Uint64ToBytes(num uint64) []byte {
+	s := strconv.FormatUint(num, 10)
+	return []byte(s)
 }
