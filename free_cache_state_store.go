ed3a5e4d4f36d9f8cca95b38f13d58f7dca20d95
Fan Tianhao <70184655+TTraveller7@users.noreply.github.com>
2024-02-20 18:47:24 +0800

    change scrape interval, add port forwarding, fix cat command (#40)

--- free_cache_state_store.go
+++ free_cache_state_store.go
@@ -36,11 +36,21 @@ func (f *FreeCacheStateStore) Delete(ctx context.Context, key string) error {
 	return nil
 }
 
-func (f *FreeCacheStateStore) Keys(ctx context.Context) ([]string, error) {
+func (f *FreeCacheStateStore) Keys(ctx context.Context, limit int) ([]string, error) {
 	iter := f.cli.NewIterator()
 	keys := make([]string, 0)
+
+	l := limit
+	if l == 0 {
+		l = int(f.cli.EntryCount())
+	}
+	count := 0
 	for entry := iter.Next(); entry != nil; {
+		if count == l {
+			break
+		}
 		keys = append(keys, string(entry.Key))
+		count++
 	}
 	return keys, nil
 }
