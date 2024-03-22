6410e31be8954d4728129754eadddd47639a4c56
Fan Tianhao <70184655+TTraveller7@users.noreply.github.com>
2024-03-22 22:56:47 +0800

    implement redis state store (#43)

--- state_store.go
+++ state_store.go
@@ -4,12 +4,6 @@ import (
 	"context"
 )
 
-var GlobalStateStoreTypes = struct {
-	Redis string
-}{
-	Redis: "Redis",
-}
-
 type StateStore interface {
 	Get(ctx context.Context, key string) ([]byte, error)
 	Put(ctx context.Context, key string, val []byte) error
